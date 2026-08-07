8906e35e0ac5fc663db78f0e63d0c120a0d0436f
sourabhchauhan2512 <sourabhchauhan2512@gmail.com>
2026-08-07 13:14:59 +0530

    Add Variables class with main method

---
+++ Variables-and-Data-Types/Variables/Variables.java
@@ -0,0 +1,13 @@
+package com.apnacollege;
+
+public class Main {
+
+   public static void main(String[] args) {
+      // Variables
+       String name = "Aman";
+       int age = 30;
+
+       String neighbour = "Akku";
+       String friend = neighbour;
+   }
+}
