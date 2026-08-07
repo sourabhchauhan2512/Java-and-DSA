1d522028ab4dbf4186cac71b6058c7310c99963c
sourabhchauhan2512 <sourabhchauhan2512@gmail.com>
2026-08-07 13:29:49 +0530

    Add program to calculate circle circumference and area

---
+++ Variables-and-Data-Types/Problems/problem2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Code2 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter radius of circle: ");
+        double radius = sc.nextDouble();
+
+        double circumference = 2 * Math.PI * radius;
+        double area = Math.PI * radius * radius;
+
+        System.out.println("Circumference = " + circumference);
+        System.out.println("Area = " + area);
+
+        sc.close();
+    }
+}
