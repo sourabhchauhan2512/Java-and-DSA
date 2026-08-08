e2d405c64caf27a099c4c9167350928aeadf8dce
sourabhchauhan2512 <sourabhchauhan2512@gmail.com>
2026-08-08 12:19:42 +0530

    Add EvenCheck class to determine even numbers

---
+++ Functions & Methods /p34.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class EvenCheck {
+
+    static boolean isEven(int n) {
+        return n % 2 == 0;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        if (isEven(n))
+            System.out.println(n + " is Even");
+        else
+            System.out.println(n + " is Not Even");
+    }
+}
