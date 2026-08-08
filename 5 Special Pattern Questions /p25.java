3e9a99bc414cd513a1438d4d19c4015a417398ec
sourabhchauhan2512 <sourabhchauhan2512@gmail.com>
2026-08-08 12:02:24 +0530

    Add Problem25 class for printing a star pattern

---
+++ 5 Special Pattern Questions /p25.java
@@ -0,0 +1,26 @@
+public class Problem25 {
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+
+            // Spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            // Stars
+            for (int j = 1; j <= n; j++) {
+
+                if (i == 1 || i == n || j == 1 || j == n) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.println();
+        }
+    }
+}
