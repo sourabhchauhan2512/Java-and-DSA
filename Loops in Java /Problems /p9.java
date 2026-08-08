e937f3cd2d55c6b06403748645aae84277d3e058
sourabhchauhan2512 <sourabhchauhan2512@gmail.com>
2026-08-08 11:23:37 +0530

    Add prime number checker in p9.java

---
+++ Loops in Java /Problems /p9.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Problem9 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        boolean isPrime = true;
+
+        if (n <= 1) {
+            isPrime = false;
+        } else {
+            for (int i = 2; i < n; i++) {
+                if (n % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        if (isPrime) {
+            System.out.println(n + " is a prime number.");
+        } else {
+            System.out.println(n + " is not a prime number.");
+        }
+
+        sc.close();
+    }
+}
