8e54fb9ad4202e8563c2f1b1d106fd315cfc7399
sourabhchauhan2512 <sourabhchauhan2512@gmail.com>
2026-08-08 12:03:56 +0530

    Add Problem28 class for special pattern printing

---
+++ 5 Special Pattern Questions /p28.java
@@ -0,0 +1,15 @@
+public class Problem28 {
+    public static void main(String[] args) {
+
+        int n = 4;
+
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j <= n - i + 1; j++) {
+                System.out.print(i);
+            }
+
+            System.out.println();
+        }
+    }
+}
