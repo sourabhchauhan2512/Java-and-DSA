05ec454229dc509ca87e0b28b125b65578499df1
sourabhchauhan2512 <sourabhchauhan2512@gmail.com>
2026-08-11 14:12:52 +0530

    Add eligibility check based on age input

---
+++ Functions in Java /p33.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class main {
+
+    public static boolean isEligible(int age) {
+        if(age>18) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);   
+    int age = sc.nextInt();
+
+    System.out.println(isEligible(age));
+    }
+}
