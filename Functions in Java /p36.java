be61b00afe08a6ecd1d401b2215409aa8aaf6353
sourabhchauhan2512 <sourabhchauhan2512@gmail.com>
2026-08-12 12:07:28 +0530

    Add power function to calculate x raised to n

---
+++ Functions in Java /p36.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class main {
+    public static int power(int x, int n) {
+        int result = 1;
+
+        for(int i=1; i<=n; i++) {
+            result = result * x;
+        }
+         return result;
+    }
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int x = sc.nextInt();
+    int n = sc.nextInt();
+
+    System.out.println(power(x, n));
+    }
+}
