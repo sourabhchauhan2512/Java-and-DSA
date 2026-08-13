52cfd721ffb3bc1dd6bb34f86309804a66c82015
sourabhchauhan2512 <sourabhchauhan2512@gmail.com>
2026-08-13 11:02:34 +0530

    Implement GCD function in Java

---
+++ Functions in Java /p37.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class main {
+    public static int gcd(int a, int b) {
+        int gcd = 1;
+        
+        for(int i=1; i<=a; i++) {
+            if(a % i == 0 && b % i == 0) {
+                gcd = i;
+            }
+        }
+         return gcd;
+    }
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+
+    System.out.println(gcd(a,b));
+    }
+}
